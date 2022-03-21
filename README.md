6829d8a4a76f8a57659f1ad34b3fbf6f7048b53d
William Shen <william.w.y.shen@gmail.com>
2022-03-21 17:42:17 -0400

    Clean up gym env and README

--- README.md
+++ README.md
@@ -1,14 +1,29 @@
 # firehose
-We are trying to put out big fires
+![good-coverage](https://img.shields.io/badge/coverage-101%25-brightgreen)
+
+We are trying to put out` big fires
 
 ![fine](./figs/giphy.gif)
 
-# Run the env
+## Installation
+Use a virtual environment it'll make your life easier
+
+1. Download Eigen and store it somewhere you like: http://eigen.tuxfamily.org/index.php?title=Main_Page#Download
+2. Compile and setup cell2fire
+   1. `cd Cell2Fire/cell2fire/Cell2FireC`
+   2. Edit Makefile to have the correct path to Eigen
+   3. `make`
+   4. `cd ../ && pip install -r requirements.txt`
+   5. `cd ../ && pip install -r requirements.txt` (yes repeat it twice to go up directory)
+   6. `python setup.py develop`
+3. Yay! Follow instructions below to run the environment and train an agent.
+
+## Run the env
 ```
 python cell2fire/gym_env.py
 ```
 
-# Train a PPO agent
+## Train a PPO agent
 ```
 python cell2fire/rl_experiment.py
 ```
--- cell2fire/gym_env.py
+++ cell2fire/gym_env.py
@@ -1,30 +1,42 @@
+import os
+import subprocess
+import time
+
+import cv2
+import numpy as np
+import pandas as pd
 from gym import Env
 from gym.spaces import Discrete, Box
-import numpy as np
-import subprocess
-import os
-import pandas as pd
-import cv2
-import sys
-import time
+
 from cell2fire.utils.ReadDataPrometheus import Dictionary
 
-
 ENVS = []
+_COMMAND_STR = "{} --input-instance-folder {} --output-folder {} --ignitions --sim-years 1 \
+    --nsims 1 --grids --final-grid --Fire-Period-Length 1.0 --output-messages \
+    --weather rows --nweathers 1 --ROS-CV 0.5 --IgnitionRad 0 --seed 123 --nthreads 1 \
+    --ROS-Threshold 0.1 --HFI-Threshold 0.1  --HarvestPlan"
+
 
 class FireEnv(Env):
-    def __init__(self, map="dogrib", max_steps=200, ignition_point=(0, 0), ignition_radius=0):
+    def __init__(
+        self, map="dogrib", max_steps=200, ignition_point=(0, 0), ignition_radius=0
+    ):
         # TODO: Create the process with the input map
         self.action_space = Discrete(3)
         self.observation_space = Box(low=np.array([0]), high=np.array([100]))
         self.state = [0]
-        self.base_path =  os.path.dirname(os.path.realpath(__file__))
+
+        # Set paths of all the things
+        self.base_path = os.path.dirname(os.path.realpath(__file__))
         self.binary = "{}/Cell2FireC/Cell2Fire".format(self.base_path)
         self.data_folder = "{}/../data/{}/".format(self.base_path, map)
         self.forest_datafile = "{}/../data/{}/Forest.asc".format(self.base_path, map)
         self.output_folder = "{}/../results/{}/".format(self.base_path, map)
+
+        # TODO: detect if process throws an error?
         self.fire_process = None
-        self.MAX_STEPS = max_steps
+
+        self.max_steps = max_steps
         self.forest_image_data = np.loadtxt(self.forest_datafile, skiprows=6)
 
         self.load_forest_image()
@@ -36,68 +48,90 @@ class FireEnv(Env):
     def load_forest_image(self):
         # Load in the forest image through the color lookup dict
         fb_lookup = os.path.join(self.data_folder, "fbp_lookup_table.csv")
-        self.fb_dict = Dictionary(fb_lookup)[1]        
-        self.fb_dict['-9999'] = [0,0,0]  
-        self.forest_image = np.zeros( (self.forest_image_data.shape[0], self.forest_image_data.shape[1], 3) )
+        self.fb_dict = Dictionary(fb_lookup)[1]
+        self.fb_dict["-9999"] = [0, 0, 0]
+        self.forest_image = np.zeros(
+            (self.forest_image_data.shape[0], self.forest_image_data.shape[1], 3)
+        )
         for x in range(self.forest_image_data.shape[0]):
             for y in range(self.forest_image_data.shape[1]):
-                self.forest_image[x, y] = self.fb_dict[str(int(self.forest_image_data[x, y]))][:3]
+                self.forest_image[x, y] = self.fb_dict[
+                    str(int(self.forest_image_data[x, y]))
+                ][:3]
 
-    def step(self, action):
+    def step(self, action, debug: bool = True):
+        # if debug:
+        #     print(action, "step")
+
         result = ""
         q = 0
-        while(result != "Input action"):
-            result = self.fire_process.stdout.readline().strip().decode("utf-8") 
+        while result != "Input action":
+            result = self.fire_process.stdout.readline().strip().decode("utf-8")
+            # print(result)
             # assert len(result)>0
-        value = str(action) + '\n'
-        value = bytes(value, 'UTF-8')
+        value = str(action) + "\n"
+        value = bytes(value, "UTF-8")
         self.fire_process.stdin.write(value)
         self.fire_process.stdin.flush()
 
         state_file = self.fire_process.stdout.readline().strip().decode("utf-8")
+        # FIXME: is this necessary?
         time.sleep(0.01)
-        df = pd.read_csv(state_file, sep=',',header=None)
+        df = pd.read_csv(state_file, sep=",", header=None)
         self.state = df.values
 
-        done = self.iter >= self.MAX_STEPS
+        done = self.iter >= self.max_steps
         info = {}
         reward = 0
-        self.iter+=1
+        self.iter += 1
 
         return self.state, reward, done, info
 
-    def render(self):
-        im = (self.forest_image*255).astype('uint8')
+    def render(self, **kwargs):
+        im = (self.forest_image * 255).astype("uint8")
 
         # Set fire cells
-        idxs = np.where(self.state>0)
-        im[idxs] = [0,0,255]
+        idxs = np.where(self.state > 0)
+        im[idxs] = [0, 0, 255]
 
         # Scale to be larger
-        im = cv2.resize(im, (im.shape[1]*4, im.shape[0]*4), interpolation = cv2.INTER_AREA)
+        im = cv2.resize(
+            im, (im.shape[1] * 4, im.shape[0] * 4), interpolation=cv2.INTER_AREA
+        )
         cv2.imshow("Fire", im)
         cv2.waitKey(10)
-    
 
-    def reset(self):
-        self.iter=0
+    def reset(self, **kwargs):
+        self.iter = 0
 
-        if(self.fire_process is not None):
+        if self.fire_process is not None:
             self.fire_process.kill()
 
-        command_string = "{} --input-instance-folder {} --output-folder {} --ignitions --sim-years 1 --nsims 1 --grids --final-grid --Fire-Period-Length 1.0 --output-messages --weather rows --nweathers 1 --ROS-CV 0.5 --IgnitionRad 0 --seed 123 --nthreads 1 --ROS-Threshold 0.1 --HFI-Threshold 0.1  --HarvestPlan".format(self.binary, self.data_folder, self.output_folder)
+        command_string = _COMMAND_STR.format(
+            self.binary, self.data_folder, self.output_folder
+        )
         command_string_args = command_string.split(" ")
-        self.fire_process = subprocess.Popen(command_string_args, stdout=subprocess.PIPE, stderr=subprocess.PIPE, stdin=subprocess.PIPE)
+        self.fire_process = subprocess.Popen(
+            command_string_args,
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+            stdin=subprocess.PIPE,
+        )
         self.state = [0]
         return self.state
 
-if(__name__ == "__main__"):
+
+def main():
     env = FireEnv()
     state = env.reset()
     for _ in range(500):
         action = env.action_space.sample()
         state, reward, done, info = env.step(action)
         env.render()
-        if(done):
+        if done:
             state = env.reset()
     print("Finished!")
+
+
+if __name__ == "__main__":
+    main()
