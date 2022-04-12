30d4f4fb6a9ac723c950e6264bdc5b9229e31022
Aidan Curtis <curtisa@mit.edu>
2022-04-12 16:21:06 -0400

    experiment deployment and parseargs

--- cell2fire/rl_experiment_vectorized.py
+++ cell2fire/rl_experiment_vectorized.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from stable_baselines3 import PPO, DDPG, DQN
+from stable_baselines3 import PPO, A2C, TRPO
 from stable_baselines3.common.callbacks import CheckpointCallback
 from stable_baselines3.common.env_util import make_vec_env
 from stable_baselines3.common.utils import set_random_seed
@@ -12,6 +12,7 @@ import os
 from stable_baselines3.common.monitor import Monitor
 from stable_baselines3.common.results_plotter import load_results, ts2xy
 from firehose.models import ExperimentHelper, IgnitionPoints, IgnitionPoint
+import argparse
 
 from typing import Callable
 
@@ -21,6 +22,7 @@ num_cpu = 16
 
 
 def main(
+    args,
     total_timesteps=2_000_000,
     checkpoint_save_freq=int(2_000_000 / 100),
     should_eval=False,
@@ -31,9 +33,20 @@ def main(
     print("Total timesteps:", total_timesteps)
     print("Checkpoint freq:", checkpoint_save_freq)
 
-    env_with_fixed_ignition = lambda: FireEnv(
-        ignition_points=IgnitionPoints([IgnitionPoint(1100, 1)])
-    )
+    if(args.ignition_type == "fixed"):
+        env_with_fixed_ignition = lambda: FireEnv(
+            ignition_points=IgnitionPoints([IgnitionPoint(1100, 1)]),
+            action_type = args.action_space,
+            fire_map = args.map
+        )
+    elif(args.ignition_type == "random"):
+        env_with_fixed_ignition = lambda: FireEnv(
+            action_type = args.action_space, 
+            fire_map = args.map
+        )
+    else:
+        raise NotImplementedError
+
     # Need to use use SubprocVecEnv so its parallelized. DummyVecEnv is sequential on a single core
     env = make_vec_env(
         env_with_fixed_ignition, n_envs=num_cpu, vec_env_cls=SubprocVecEnv
@@ -41,7 +54,17 @@ def main(
 
     # model = DDPG("MlpPolicy", env, verbose=1, tensorboard_log="./tmp/ddpg_static_7")
     tf_logdir = f'{tf_logdir}_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}'
-    model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=tf_logdir)
+
+    if(args.algo == "ppo"):
+        model = PPO("MlpPolicy", env, verbose=1, tensorboard_log=tf_logdir)
+    elif(args.algo == "a2c")
+        model = A2C("MlpPolicy", env, verbose=1, tensorboard_log=tf_logdir)
+    elif(args.algo == "trpo"):
+        model = TRPO("MlpPolicy", env, verbose=1, tensorboard_log=tf_logdir)
+    else:
+        raise NotImplementedError
+
+
     print("Tensorboard logdir:", tf_logdir)
     # model = DQN("MlpPolicy", env, verbose=1, tensorboard_log="./tmp/dqn_static_7")
     checkpoint_callback = CheckpointCallback(
@@ -75,4 +98,11 @@ def main(
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-a", "--algo", default="ppo", help="Specifies the RL algorithm to use")
+    parser.add_argument("-a", "--map", default="20x20", help="Specifies the map to run the environment in")
+    parser.add_argument("-i", "--ignition_type", default="fixed", help="Specifies whether to use a random or fixed fire ignitinon point")
+    parser.add_argument("-p", "--preharvest", default="fixed", help="Specifies whether or not to harvest before fire ignition")
+    parser.add_argument("-s", "--seed", default="0", help="RL seed")
+    args = parser.parse_args()
+    main(args)
---
+++ deploy_experiments.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task 16
+#SBATCH --time=12:00:00
+#SBATCH --mem-per-cpu=16000
+
+
+# TODO: curtisa
+i=1
+for algo in "a2c" "ppo" "trpo" # "random" "naive" # TODO: Will
+do
+	for map in "20x20" "Harvest40x40" "dogrib"
+	do
+		for ignition_mode in "fixed" #"random" # TODO: Will
+		do
+			for action_space in "xy" "flat"
+			do
+				for seed in "1" "2" "3"
+				do
+					if [ $((i)) -eq  $((SLURM_ARRAY_TASK_ID + 0)) ]; then
+						python cell2fire/rl_experiment_vectorized.py --algo="$algo" --map="$map" --ignition_mode="$ignition_mode" --action_space=$action_space --seed=$seed
+					fi
+					i=$((i+1))
+			    done
+			done
+		done
+	done
+done
+
+echo $i
+echo ${SLURM_ARRAY_TASK_ID}
